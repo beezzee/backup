7c6427286a221c3dd9b8a11d40ca6258405909b3
beezzee <>
2025-12-19 18:59:00 +0100

    Add .gitignore and extend test

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 touch src/dir1/updated_file
-python3 ../backup.py -c test.yaml backup
+python ../backup.py -c test.yaml backup
+python ../backup.py -c test.yaml list-backups
 echo "Manually check that inodes of fixed_file are fixed between runs"
 find -printf "%p %i\n"
